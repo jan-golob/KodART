e65c92fda4823d6dc399652098868e4244640d64
jan-golob <jangolob.jg@gmail.com>
2016-05-16 09:45:26 +0200

    Popravi modul da ne vrne še več errorov

---
+++ src/Abstraktnost/Level.java
@@ -0,0 +1,17 @@
+package Abstraktnost;
+
+import java.awt.Color;
+
+public class Level extends IzborBarve {
+
+	public Level() {
+		super(3);
+	}
+
+	@Override
+	public Color eval(float x, float y) {
+		
+		return null;
+	}
+
+}
---
+++ src/Abstraktnost/Modul.java
@@ -0,0 +1,21 @@
+package Abstraktnost;
+
+import java.awt.Color;
+
+public class Modul extends IzborBarve {
+
+	public Modul() {
+		super(2);
+	}
+
+	@Override
+	public Color eval(float x, float y) {
+	Color c0 = this.podizbori[0].eval(x, y);
+	Color c1 = this.podizbori[1].eval(x, y);
+	float r = (float) (c0.getRed()%((c1.getRed()+1)/1.004));
+	float g = (float) (c0.getGreen()%((c1.getGreen()+1)/1.004));
+	float b = (float) (c0.getBlue()%((c1.getBlue()+1)/1.004));
+	return new Color(r, g, b);
+	}
+
+}
