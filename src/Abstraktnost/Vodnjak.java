3cd015114edc901ed3aa99bb51f3de038d178b6c
Mihec123 <mihavsec@gmail.com>
2016-05-18 12:32:01 +0200

    Class vodnjak

--- src/Abstraktnost/Vodnjak.java
+++ src/Abstraktnost/Vodnjak.java
@@ -8,14 +8,14 @@ public class Vodnjak extends IzborBarve {
 		super(1);
 	}
 	
-	private float vodnjak(float x){
+	private float vodnak(float x){
 		return (float) (1 - 2 / Math.pow((1 + x*x),8));
 		
 	}
 
 	@Override
 	public Color eval(float x, float y) {
-		return new Color(vodnjak(this.podizbori[0].eval(x,y).getRed()),vodnjak(this.podizbori[0].eval(x,y).getGreen()),vodnjak(this.podizbori[0].eval(x,y).getBlue()));
+		return new Color(vodnak(this.podizbori[0].eval(x,y).getRed()),vodnak(this.podizbori[0].eval(x,y).getGreen()),vodnak(this.podizbori[0].eval(x,y).getBlue()));
 	}
 
 }
